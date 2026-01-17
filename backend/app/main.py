d43515863ae6eee1d9d36694a5d3fe652892efe1
trngnneee <dtn06052005@gmail.com>
2026-01-17 18:24:02 +0700

    Fix CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,6 +21,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
+        "https://datastorm-frontend.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"], 
